6b89a27e62e32f4db772351f6eaf21bf067b129a
Michael Dunkle <michael.dunkle@gmail.com>
2017-05-23 10:57:55 -0600

    Gets basic flight search working

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+// Do boot up stuff
 console.log('...booting up');
+require('dotenv').config()
+
+const request = require('request');
+
+const apiKey = process.env.SKYSCANNER_KEY;
+
+const url='us/anywhere/anytime/anytime';
+request(`http://partners.api.skyscanner.net/apiservices/browseroutes/v1.0/US/usd/en-US/${url}?apikey=${apiKey}`, function (err, res, body) {
+  if (err) {
+    console.error('error:', err);
+  }
+  console.log(body);
+});

09148c7ad0f347e31d5e258ca99327e710e6e069
Michael Dunkle <michael.dunkle@gmail.com>
2017-05-26 16:52:10 -0600

    Adds module to lookup places

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-// Do boot up stuff
-console.log('...booting up');
-require('dotenv').config()
+require('dotenv').config();
 
 const request = require('request');
 const yargs = require('yargs');
 
 const dates = require('./lib/dates.js');
+const locations = require('./lib/locations.js');
 
 const apiKey = process.env.SKYSCANNER_KEY;
 
@@ -13,13 +12,15 @@ const argv = yargs.argv;
 var command = argv._[0];
 
 if (command === 'search') {
-  var formattedDates = dates.formatDates(argv.depart, argv.return);
-  const url=`${argv.home}/anywhere/${formattedDates.dep}/${formattedDates.ret}`;
-  request(`http://partners.api.skyscanner.net/apiservices/browseroutes/v1.0/US/usd/en-US/${url}?apikey=${apiKey}`, function (err, res, body) {
-    if (err) {
-      console.error('error:', err);
-    }
-    console.log(body);
+  locations.findPlace(argv.home, (err, place) => {
+    var formattedDates = dates.formatDates(argv.depart, argv.return);
+    const url=`${place}/anywhere/${formattedDates.dep}/${formattedDates.ret}`;
+    request(`http://partners.api.skyscanner.net/apiservices/browseroutes/v1.0/US/usd/en-US/${url}?apikey=${apiKey}`, function (err, res, body) {
+      if (err) {
+        console.error('error:', err);
+      }
+      console.log(body);
+    });
   });
 } else {
   console.log('You need to provide a command!');
